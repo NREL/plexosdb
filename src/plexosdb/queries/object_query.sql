f98f569be4c813fcab096d4d291c12f8c992d75e
Kamran Tehranchi <83722342+ktehranchi@users.noreply.github.com>
2024-08-13 12:42:47 -0700

    Property Parsing Updates (#3)

--- src/plexosdb/queries/object_query.sql
+++ src/plexosdb/queries/object_query.sql
@@ -3,93 +3,139 @@ WITH scenario_cte AS (
         obj.name,
         obj.object_id,
         tag.data_id,
-        mem.collection_id
+        mem.collection_id,
+        cat.name AS category_name
     FROM
         t_membership AS mem
     INNER JOIN t_tag AS tag ON
         tag.object_id = mem.child_object_id
     INNER JOIN t_object AS obj ON
         mem.child_object_id = obj.object_id
+    LEFT JOIN t_category AS cat ON
+    	obj.category_id = cat.category_id
     WHERE
         mem.child_class_id = 78
-        AND mem.collection_id IN (1, 698, 706, 700) -- Collections belong to scenarios
+        AND mem.collection_id IN (1, 698, 706, 701) -- Collections belong to scenarios
 ),
 text_cte AS (
     SELECT
+        base_gen_data.data_id AS parent_object_data_id,
         obj.object_id,
-        obj.name AS nested_object,
+        obj.name AS nested_object_name,
         prop.name AS nested_property_name,
-        text.value AS text,
-        d.data_id,
-        d.value,
-        tag.data_id AS tag_data_id,
+        memo.value AS memo,
+        text.value as text_value,
+        text_class.name ,
         date_from.date AS date_from,
         date_to.date AS date_to,
-        memo.value AS memo,
+        text.action_id,        
+        tagged_object.name AS tagged_object_name,
         scenario.name AS scenario,
-        text.class_id AS text_class_id,
-        text.action_id AS text_action_id,
-        class_text.name AS class_text_name,
-        action.action_symbol AS action_symbol
+        CASE 
+            WHEN text_class.name IN ('Data File') THEN 'Data File'
+            WHEN text_class.name IN ('Timeslice') THEN 'Timeslice'
+        END AS text_class_type
     FROM
         t_membership AS mem
-    LEFT JOIN t_data AS d ON
-        mem.membership_id = d.membership_id
-    LEFT JOIN t_property AS prop ON
-        prop.property_id = d.property_id
-    LEFT JOIN t_memo_data AS memo ON
-        memo.data_id = d.data_id
-    LEFT JOIN t_date_from AS date_from ON
-        d.data_id = date_from.data_id
-    LEFT JOIN t_date_to AS date_to ON
-        d.data_id = date_to.data_id
-    INNER JOIN t_text AS text ON
-        text.data_id = d.data_id
     INNER JOIN t_object AS obj ON
         mem.child_object_id = obj.object_id
+    LEFT JOIN t_data AS base_gen_data ON
+        mem.membership_id = base_gen_data.membership_id
+    LEFT JOIN t_property AS prop ON
+        prop.property_id = base_gen_data.property_id
+    LEFT JOIN t_memo_data AS memo ON
+        memo.data_id = base_gen_data.data_id
+    LEFT JOIN t_date_from AS date_from ON
+        base_gen_data.data_id = date_from.data_id
+    LEFT JOIN t_date_to AS date_to ON
+        base_gen_data.data_id = date_to.data_id
+    ----- nested text data
     INNER JOIN t_tag AS tag ON
-        tag.object_id = obj.object_id
-    LEFT JOIN t_class AS class_text ON
-        text.class_id = class_text.class_id
+        tag.data_id = base_gen_data.data_id
+    LEFT JOIN t_object AS tagged_object ON
+        tagged_object.object_id = tag.object_id
+    LEFT JOIN t_class AS tagged_object_class ON
+        tagged_object.class_id = tagged_object_class.class_id
     LEFT JOIN t_action AS action ON
-        text.action_id = action.action_id
+        action.action_id = tag.action_id
+    -- bring in the text object 
+    INNER JOIN t_membership AS text_tag_obj_mem ON
+        text_tag_obj_mem.child_object_id = tagged_object.object_id
+    INNER JOIN t_data AS text_tag_obj_data ON
+        text_tag_obj_mem.membership_id = text_tag_obj_data.membership_id
+    INNER JOIN t_text AS text ON
+        text.data_id = text_tag_obj_data.data_id 
+    LEFT JOIN t_class AS text_class on
+        text.class_id = text_class.class_id
     LEFT JOIN scenario_cte AS scenario ON
-        d.data_id = scenario.data_id
+        base_gen_data.data_id = scenario.data_id
+    WHERE text_class.name IN ('Data File', 'Timeslice')
+),
+tag_cte AS (
+SELECT
+    base_gen_data.data_id AS parent_object_data_id,
+    obj.object_id,
+    obj.name AS nested_object_name,
+    prop.name AS nested_property_name,
+    tagged_object.name AS tagged_object_name,
+    tagged_object_class.name AS tagged_obj_class_name,
+    action.action_symbol AS action_symbol
+FROM
+    t_membership AS mem
+INNER JOIN t_object AS obj ON
+    mem.child_object_id = obj.object_id
+LEFT JOIN t_data AS base_gen_data ON
+    mem.membership_id = base_gen_data.membership_id
+LEFT JOIN t_property AS prop ON
+    prop.property_id = base_gen_data.property_id
+INNER JOIN t_tag AS tag ON
+    tag.data_id = base_gen_data.data_id
+LEFT JOIN t_object AS tagged_object ON
+    tagged_object.object_id = tag.object_id
+LEFT JOIN t_class AS tagged_object_class ON
+    tagged_object.class_id = tagged_object_class.class_id
+LEFT JOIN t_action AS action ON
+    action.action_id = tag.action_id
+WHERE tagged_obj_class_name == "Variable"
 )
 SELECT
-    class_parent.class_id AS parent_class_id,
-    mem.parent_object_id as parent_object_id,
+    mem.membership_id,
+    mem.parent_object_id AS parent_object_id,
+    parent_object.name AS parent_object_name,
     class_parent.name AS parent_class,
-    class_child.class_id AS child_class_id,
     class_child.name AS child_class,
     cat.name AS category,
-    child_obj.object_id as object_id,
-    child_obj.name as object_name,
+    child_obj.object_id AS child_object_id,
+    child_obj.name AS child_object_name,
     prop.name AS property_name,
     unit.value AS property_unit,
-    data.value as property_value,
-    IFNULL(band.band_id, 1) as band,
-    COALESCE(date_from.date, nested_object.date_from) as date_from,
-    COALESCE(date_to.date, nested_object.date_to) as date_to,
-    COALESCE(memo.value, nested_object.memo) as memo,
-    COALESCE(scenario.name, nested_object.scenario) AS scenario,
-    MAX(CASE WHEN class_text_name = 'Data File' THEN text END) AS data_file,
-    MAX(CASE WHEN class_text_name = 'Variable' THEN text END) AS variable,
-    MAX(CASE WHEN class_text_name = 'Timeslice' THEN text END) AS timeslice,
-    nested_object.action_symbol AS action_symbol
+    data.value AS property_value,
+    IFNULL(band.band_id, 1) AS band,
+    COALESCE(date_from.date, nested_object_df.date_from) AS date_from,
+    COALESCE(date_to.date, nested_object_df.date_to) AS date_to,
+    COALESCE(memo.value, nested_object_df.memo) AS memo,
+    scenario.category_name AS scenario_category,
+    COALESCE(scenario.name, nested_object_df.scenario) AS scenario,
+    nested_variable_object.action_symbol AS action_symbol,
+	nested_object_df.tagged_object_name AS data_file_tag,
+	nested_object_df.text_value AS data_file,
+	nested_variable_object.tagged_object_name AS varible_tag,
+	nested_object_ts.tagged_object_name AS timeslice_tag,
+	nested_object_ts.text_value AS timeslice
 FROM
     t_membership AS mem
 LEFT JOIN t_class AS class_parent ON
     mem.parent_class_id = class_parent.class_id
-LEFT JOIN t_class AS class_child ON
-    mem.child_class_id = class_child.class_id
-LEFT JOIN t_collection AS collection ON
-    collection.collection_id = mem.collection_id
+LEFT JOIN t_object AS parent_object ON
+	mem.parent_object_id = parent_object.object_id
 LEFT JOIN t_object AS child_obj ON
     child_obj.object_id = mem.child_object_id
-LEFT JOIN t_object AS parent_obj ON
-    parent_obj.object_id = mem.parent_object_id
-LEFT JOIN t_data AS data ON
+LEFT JOIN t_class AS class_child ON
+    mem.child_class_id = class_child.class_id
+LEFT JOIN t_category AS cat ON 
+    child_obj.category_id = cat.category_id
+-------- property data -----------------------
+LEFT JOIN t_data AS data ON 
     data.membership_id = mem.membership_id
 LEFT JOIN t_memo_data AS memo ON
     memo.data_id = data.data_id
@@ -103,27 +149,14 @@ LEFT JOIN t_unit AS unit ON
     unit.unit_id = prop.unit_id
 LEFT JOIN t_band AS band ON
     data.data_id = band.data_id
-LEFT JOIN text_cte AS nested_object ON
-    data.data_id = nested_object.tag_data_id
+------------ CTE Fields ------------
 LEFT JOIN scenario_cte AS scenario ON
     scenario.data_id = data.data_id
-LEFT JOIN t_category AS cat ON
-    child_obj.category_id = cat.category_id
-GROUP BY
-    class_parent.class_id,
-    mem.parent_object_id,
-    class_parent.name,
-    class_child.class_id,
-    class_child.name,
-    cat.name,
-    child_obj.object_id,
-    child_obj.name,
-    prop.name,
-    unit.value,
-    data.value,
-    IFNULL(band.band_id, 1),
-    COALESCE(date_from.date, nested_object.date_from),
-    COALESCE(date_to.date, nested_object.date_to),
-    COALESCE(memo.value, nested_object.memo),
-    COALESCE(scenario.name, nested_object.scenario),
-    nested_object.action_symbol;
+LEFT JOIN text_cte AS nested_object_df ON
+    data.data_id = nested_object_df.parent_object_data_id
+	AND nested_object_df.text_class_type = 'Data File'
+LEFT JOIN text_cte AS nested_object_ts ON
+    data.data_id = nested_object_ts.parent_object_data_id
+	AND nested_object_ts.text_class_type = 'Timeslice'
+LEFT JOIN tag_cte AS nested_variable_object ON
+	data.data_id = nested_variable_object.parent_object_data_id
