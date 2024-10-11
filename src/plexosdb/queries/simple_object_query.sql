4ff1e2ab71032f67e288d5482dc0ecd2c8193905
pesap <pesapsanchez@gmail.com>
2024-10-11 02:04:12 -0600

    fix: Cleaning the query

--- src/plexosdb/queries/simple_object_query.sql
+++ src/plexosdb/queries/simple_object_query.sql
@@ -1,9 +1,9 @@
 -- Query to get all the objects and nested objects
 -- We do not include scenario tags on nested objects.
-WITH scenario_cte as (
+WITH scenario_cte AS (
 SELECT
-	obj.name,
-	obj.object_id,
+	object.name,
+	object.object_id,
 	tag.data_id,
 	mem.collection_id,
 	  cat.name AS category_name
@@ -11,10 +11,10 @@ FROM
 	t_membership AS mem
 LEFT JOIN t_tag AS tag ON
 	tag.object_id = mem.child_object_id
-LEFT JOIN t_object AS obj ON
-	mem.child_object_id = obj.object_id
+LEFT JOIN t_object AS object ON
+	mem.child_object_id = object.object_id
 LEFT JOIN t_category AS cat ON
-	obj.category_id = cat.category_id
+	object.category_id = cat.category_id
 WHERE
 	mem.collection_id in
         (
@@ -29,46 +29,67 @@ WHERE
         )
  )
 SELECT
-	obj.name,
-	obj.object_id,
-	m.collection_id,
-	m.membership_id,
-	prop.name,
-	data.value AS value,
-	date_from.date as date_from,
-	date_to.date as date_to,
-	text.value as text_name,
-	text.class_id as text_class_id,
-	tag.data_id as tag_data_id,
-	tag_object.name as tag_object,
-	tag.object_id as tag_object_id,
-	tag_class.name as tag_class_name,
-	scenario.name AS scenario,
-	scenario.object_id AS scenario_obj_id,
-	scenario.data_id AS scenario_data_id,
-	scenario.collection_id AS scenario_collection_id
+	parent_class.name AS parent_class_name,
+	child_class.name AS child_class_name,
+	parent_object.object_id AS parent_obj_id,
+	object.object_id AS object_id,
+	object.name AS name,
+	category.name AS category,
+	property.name AS property_name,
+	unit.value AS property_unit,
+	data.value AS property_value,
+	IFNULL(band.band_id, 1) AS band,
+	date_from.date AS date_from,
+	date_to.date AS date_to,
+	text.value AS text,
+	text_class.name AS text_class_name,
+	tag.data_id AS tag_data_id,
+	tag_object.name AS tag_object,
+	tag.object_id AS tag_object_id,
+	tag_class.name AS tag_class_name,
+	action.action_symbol as action,
+	scenario.name AS scenario
+	-- scenario.object_id AS scenario_obj_id,
+	-- scenario.data_id AS scenario_data_id,
+	-- scenario.collection_id AS scenario_collection_id
 FROM
-	t_membership m
-LEFT JOIN t_object AS obj ON
-	m.child_object_id = obj.object_id
-LEFT JOIN t_data data ON
-	m.membership_id = data.membership_id
-LEFT JOIN t_property as prop ON
-	data.property_id = prop.property_id
+	t_membership membership
+LEFT JOIN t_object AS object ON
+	membership.child_object_id = object.object_id
+LEFT JOIN t_category AS category ON
+	object.category_id = category.category_id
+LEFT JOIN t_object AS parent_object ON
+	membership.parent_object_id = parent_object.object_id
+LEFT JOIN t_class AS child_class ON
+	membership.child_class_id = child_class.class_id
+LEFT JOIN t_class AS parent_class ON
+	membership.parent_class_id = parent_class.class_id
+LEFT JOIN t_data AS data ON
+	membership.membership_id = data.membership_id
+LEFT JOIN t_band AS band on
+	data.data_id = band.band_id
+LEFT JOIN t_property AS property ON
+	data.property_id = property.property_id
+LEFT JOIN t_unit AS unit ON
+	property.unit_id = unit.unit_id
 LEFT JOIN t_date_from AS date_from ON
 	data.data_id = date_from.data_id
 LEFT JOIN t_date_to AS date_to ON
 	data.data_id = date_to.data_id
-LEFT JOIN t_tag as tag ON
+LEFT JOIN t_tag AS tag ON
 	data.data_id = tag.data_id
-LEFT JOIN scenario_cte as scenario ON
+LEFT JOIN t_action AS action ON
+	tag.action_id = action.action_id
+LEFT JOIN scenario_cte AS scenario ON
 	scenario.data_id = data.data_id
 	and tag.data_id = scenario.data_id
-LEFT JOIN t_text as text on
+LEFT JOIN t_text AS text on
 	text.data_id = data.data_id
-LEFT JOIN t_object as tag_object ON
+LEFT JOIN t_class AS text_class on
+	text.class_id = text_class.class_id
+LEFT JOIN t_object AS tag_object ON
 	tag_object.object_id = tag.object_id
-LEFT JOIN t_class as tag_class ON
+LEFT JOIN t_class AS tag_class ON
 	tag_class.class_id = tag_object.class_id
 WHERE
 	(tag_class.name ISNULL
