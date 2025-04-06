1fbdda083acdee05d913544d7b0ab3ca39dd204e
pesap <pesap@users.noreply.github.com>
2025-04-06 15:36:42 -0600

    fix: Adding further testing

--- tests/test_plexosdb_scenario.py
+++ tests/test_plexosdb_scenario.py
@@ -1,4 +1,7 @@
+import pytest
+
 from plexosdb.db import PlexosDB
+from plexosdb.exceptions import NameError
 
 
 def test_adding_scenaro(db_instance_with_schema):
@@ -10,3 +13,7 @@ def test_adding_scenaro(db_instance_with_schema):
 
     scenario_id = db.get_scenario_id(test_scenario)
     assert object_id == scenario_id
+
+    test_scenario = "Test"
+    with pytest.raises(NameError):
+        _ = db.add_scenario(test_scenario)
